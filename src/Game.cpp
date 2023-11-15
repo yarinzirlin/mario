92204dd6f38d4bb76efddfeb8e68c5803a17f888
yarinz <yarzzx3@gmail.com>
2023-11-15 09:34:45 +0200

    123

--- src/Game.cpp
+++ src/Game.cpp
@@ -57,7 +57,7 @@ tmx::Map map;
 #define PORTALLABLE_LAYER "Portallable"
 #define COLLISION_LAYER "Collision"
 void Game::Run() {
-  map.load("assets/maps/3.tmx");
+  map.load("assets/maps/5.tmx");
   DEBUGLOG(map.getTileSize())
 
   while (window_->isOpen()) {
